2ad619ef70e42bc6d53a6f2b0a01238d6b3eb74c
MinkoTiminko <103176063+MinkoTiminko@users.noreply.github.com>
2022-04-13 08:08:13 +0200

    Initial commit

--- src/main/java/Primitvnedatovetipy/CeleCisla.java
+++
@@ -1,9 +0,0 @@
-package Primitvnedatovetipy;
-
-public class CeleCisla {
-    public static void  main(String[] volaco){
-
-        float maeCeleCislo= 33.54F;
-        double VelkeCeleCislo  = 43.34650;
-    }
-}
